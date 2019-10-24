ce5b4712334443fd2a7d02d66d936b74865f2e4a
jjzcru <jjzcru@gmail.com>
2019-10-24 11:48:33 -0500

    Add all.js

---
+++ all.js
@@ -0,0 +1,42 @@
+const app = require('./app');
+const group = require('./group-fonts');
+
+compile()
+    .then(()=> console.log('Success'))
+    .catch(err => console.error(err));
+
+async function compile() {
+    const transform = [
+        {
+            in: 'ttf',
+            out: 'woff'
+        },
+        {
+            in: 'ttf',
+            out: 'woff2'
+        },
+        {
+            in: 'otf',
+            out: 'woff'
+        },
+        {
+            in: 'otf',
+            out: 'woff2'
+        }
+    ];
+
+    for(const item of transform) {
+        const inType = item.in;
+        const outType = item.out;
+
+        const outputFiles = await app.run(inType, outType);
+        console.log(`Transform '${inType}' files to '${outType}':`);
+        console.log(`--------------------------------------------`);
+        for (let file of outputFiles) {
+            console.log(file);
+        }
+        console.log(`--------------------------------------------`);
+    }
+
+    await group.run()
+}
