c1a43def753fe36f1e9ce9b9af029c89b490e06d
Jose Juan <josejuan2412@hotmail.com>
2023-07-12 13:13:56 -0500

    feat: :sparkles: Replace file name if it doesnt match postscript name

--- app.js
+++ app.js
@@ -1,67 +1,94 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const ttf2Woff = require('./ttf-to');
-const otf2Woff = require('./otf-to');
+const Font = require("fonteditor-core").Font;
+
+const ttf2Woff = require("./ttf-to");
+const otf2Woff = require("./otf-to");
 
 const inType = process.env.IN_TYPE;
 const outType = process.env.OUT_TYPE;
 
 run(inType, outType)
-	.then((outputFiles) => {
-		if (!!inType && !!outType) {
-			console.log(`Transform '${inType}' files to '${outType}':`);
-			console.log(`--------------------------------------------`);
-			for (let file of outputFiles) {
-				console.log(file);
-			}
-			console.log(`--------------------------------------------`);
-		}
-	})
-	.catch((err) => console.error(err));
+  .then((outputFiles) => {
+    if (!!inType && !!outType) {
+      console.log(`Transform '${inType}' files to '${outType}':`);
+      console.log(`--------------------------------------------`);
+      for (let file of outputFiles) {
+        console.log(file);
+      }
+      console.log(`--------------------------------------------`);
+    }
+  })
+  .catch((err) => console.error(err));
 
 async function run(inType, outType) {
-	if (!inType || !outType) {
-		return;
-	}
-	const inDir = path.join(__dirname, 'In');
-	const outDir = path.join(__dirname, 'Out');
+  if (!inType || !outType) {
+    return;
+  }
+  const inDir = path.join(__dirname, "In");
+  const outDir = path.join(__dirname, "Out");
 
-	const inputFonts = await getPostcripNames(getFonts(inType, inDir));
-	let outputFiles = [];
-	if (inType === 'otf' && outType === 'woff') {
-		outputFiles = await otf2Woff.otfsToWoff(inputFonts, outDir);
-	} else if (inType === 'otf' && outType === 'woff2') {
-		outputFiles = await otf2Woff.otfsToWoff2(inputFonts, outDir);
-	} else if (inType === 'ttf' && outType === 'woff') {
-		outputFiles = await ttf2Woff.ttfsToWoff(inputFonts, outDir);
-	} else if (inType === 'ttf' && outType === 'woff2') {
-		outputFiles = await ttf2Woff.ttfsToWoff2(inputFonts, outDir);
-	}
+  const inputFonts = await getPostcripNames(getFonts(inType, inDir));
+  let outputFiles = [];
+  if (inType === "otf" && outType === "woff") {
+    outputFiles = await otf2Woff.otfsToWoff(inputFonts, outDir);
+  } else if (inType === "otf" && outType === "woff2") {
+    outputFiles = await otf2Woff.otfsToWoff2(inputFonts, outDir);
+  } else if (inType === "ttf" && outType === "woff") {
+    outputFiles = await ttf2Woff.ttfsToWoff(inputFonts, outDir);
+  } else if (inType === "ttf" && outType === "woff2") {
+    outputFiles = await ttf2Woff.ttfsToWoff2(inputFonts, outDir);
+  }
 
-	for(const font of inputFonts) {
-		fs.copyFileSync(font, path.join(outDir, path.basename(font)));
-	}
+  for (const font of inputFonts) {
+    fs.copyFileSync(font, path.join(outDir, path.basename(font)));
+  }
 
-	return outputFiles;
+  return outputFiles;
 }
 
 function getFonts(extension, inDir) {
-	const inputs = fs.readdirSync(inDir);
+  const inputs = fs.readdirSync(inDir);
 
-	return inputs
-		.filter((font) => {
-			const regexString = `(.)+\.${extension}`;
-			return new RegExp(regexString, 'gi').test(font);
-		})
-		.map((font) => `${path.join(inDir, font)}`);
+  return inputs
+    .filter((font) => {
+      const regexString = `(.)+\.${extension}`;
+      return new RegExp(regexString, "gi").test(font);
+    })
+    .map((font) => `${path.join(inDir, font)}`);
 }
 
-// TODO Implement postcript transformation
 async function getPostcripNames(inputFiles) {
-	return inputFiles;
+  for (let index = 0; index < inputFiles.length; index++) {
+    let file = inputFiles[index];
+    const font = fs.readFileSync(file);
+    const fontInfo = getFont(font);
+    const postcript = fontInfo.name.postScriptName;
+    const filename = path.parse(file).name;
+    const extension = file.split(".").pop();
+    const newFile = `${path.dirname(file)}/${postcript}.${extension}`;
+
+    if (postcript !== filename) {
+      await fs.rename(file, newFile, () => {
+        console.log("NEW FILE: ", newFile);
+      });
+      inputFiles[index] = newFile;
+    }
+  }
+  return inputFiles;
 }
 
+function getFont(font) {
+  return Font.create(font, {
+    type: "ttf",
+    subset: [65, 66],
+    hinting: true,
+    compound2simple: true,
+    inflate: null,
+    combinePath: false,
+  }).get();
+}
 module.exports = {
-	run: run,
+  run: run,
 };
