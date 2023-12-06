d0d655b732ccf0d9c712491ae1fd69d879fe1525
Jose Juan <josejuan2412@hotmail.com>
2023-12-06 16:45:11 -0500

    feat: :sparkles: group fonts into folders

--- all.js
+++ all.js
@@ -1,34 +1,39 @@
-const app = require('./app');
-const group = require('./group-fonts');
+const app = require("./app");
+const group = require("./group-fonts");
 
 compile()
-    .then(()=> console.log('Success'))
-    .catch(err => console.error(err));
+  .then(() => console.log("Success"))
+  .catch((err) => console.error(err));
 
 async function compile() {
-    const transform = [
-        {
-            in: 'ttf',
-            out: 'woff2'
-        },
-        {
-            in: 'otf',
-            out: 'woff2'
-        }
-    ];
+  const transform = [
+    {
+      in: "ttf",
+      out: "woff2",
+    },
+    {
+      in: "otf",
+      out: "woff2",
+    },
+  ];
 
-    for(const item of transform) {
-        const inType = item.in;
-        const outType = item.out;
-        console.log(`Transform '${inType}' files to '${outType}':`);
-        console.log(`--------------------------------------------`);
-        const outputFiles = await app.run(inType, outType);
-        
-        for (let file of outputFiles) {
-            console.log(file);
-        }
-        console.log(`--------------------------------------------`);
+  const flag = process.env.OPT_GROUPS;
+
+  for (const item of transform) {
+    const inType = item.in;
+    const outType = item.out;
+    console.log(`Transform '${inType}' files to '${outType}':`);
+    console.log(`--------------------------------------------`);
+    const outputFiles = await app.run(inType, outType);
+
+    for (let file of outputFiles) {
+      console.log(file);
     }
-
-    await group.run()
+    console.log(`--------------------------------------------`);
+  }
+  if (!flag) {
+    await group.run();
+    return;
+  }
+  await group.runGroups();
 }
--- group-fonts.js
+++ group-fonts.js
@@ -1,62 +1,74 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const outDir = path.join(__dirname, 'Out');
-
-run();
+const outDir = path.join(__dirname, "Out");
+const inDir = path.join(__dirname, "In");
+//run();
 
 function run() {
-	const fonts = getFonts();
-	createFontsDir(fonts);
-	copyFontsToDir(fonts);
+  const fonts = getFonts(outDir);
+  createFontsDir(fonts, outDir);
+  copyFontsToDir(fonts, outDir);
 }
 
-function createFontsDir(fonts) {
-	for (let font of fonts) {
-		const fontPath = path.join(outDir, font);
-		if (!fs.existsSync(fontPath)) {
-			fs.mkdirSync(fontPath);
-		}
-	}
+function runGroups() {
+  const folders = getFolders(inDir);
+  for (const folder of folders) {
+    const fonts = getFonts(path.join(outDir, folder));
+    createFontsDir(fonts, path.join(outDir, folder));
+    copyFontsToDir(fonts, path.join(outDir, folder));
+  }
 }
 
-function copyFontsToDir(fonts) {
-	const fontTypes = ['woff', 'woff2', 'ttf', 'otf'];
-	for (let font of fonts) {
-		const baseFontPath = path.join(outDir, font);
-		for (const type of fontTypes) {
-			const fontPath = path.join(outDir, `${font}.${type}`);
-			if (fs.existsSync(fontPath)) {
-				switch (type) {
-					case 'otf':
-					case 'ttf':
-						fs.renameSync(
-							fontPath,
-							path.join(baseFontPath, `${font}.${type}`)
-						);
-						break;
-					default:
-						fs.renameSync(
-							fontPath,
-							path.join(baseFontPath, `webfont.${type}`)
-						);
-						break;
-				}
-			}
-		}
-	}
+function createFontsDir(fonts, destination) {
+  for (let font of fonts) {
+    const fontPath = path.join(destination, font);
+    if (!fs.existsSync(fontPath)) {
+      fs.mkdirSync(fontPath);
+    }
+  }
 }
 
-function getFonts() {
-	let fontsPaths = fs.readdirSync(outDir);
-	const fonts = fontsPaths
-		.filter((font) => /(.)+\.(woff|woff2)+/gi.test(font))
-		.map((font) => font.replace(/\.[^/.]+$/, ''))
-		.filter((value, index, self) => self.indexOf(value) === index);
+function copyFontsToDir(fonts, destination) {
+  const fontTypes = ["woff", "woff2", "ttf", "otf"];
+  for (let font of fonts) {
+    const baseFontPath = path.join(destination, font);
+    for (const type of fontTypes) {
+      const fontPath = path.join(destination, `${font}.${type}`);
+      if (fs.existsSync(fontPath)) {
+        switch (type) {
+          case "otf":
+          case "ttf":
+            fs.renameSync(fontPath, path.join(baseFontPath, `${font}.${type}`));
+            break;
+          default:
+            console.log(fontPath);
+            fs.renameSync(fontPath, path.join(baseFontPath, `webfont.${type}`));
+            break;
+        }
+      }
+    }
+  }
+}
 
-	return fonts;
+function getFonts(folder) {
+  let fontsPaths = fs.readdirSync(folder);
+  const fonts = fontsPaths
+    .filter((font) => /(.)+\.(woff|woff2)+/gi.test(font))
+    .map((font) => font.replace(/\.[^/.]+$/, ""))
+    .filter((value, index, self) => self.indexOf(value) === index);
+
+  return fonts;
+}
+
+function getFolders(inDir) {
+  const folders = fs
+    .readdirSync(inDir)
+    .filter((file) => fs.lstatSync(path.join(inDir, file)).isDirectory());
+  return folders;
 }
 
 module.exports = {
-	run: run,
+  run: run,
+  runGroups: runGroups,
 };
